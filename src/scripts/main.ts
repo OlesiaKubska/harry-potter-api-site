c9bfd84327b4262e908f559268d052ed57be57f0
OlesiaKubska <kublesia0908@gmail.com>
2025-05-04 23:58:16 +0200

    Added and integrated the characters section with styles

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,15 @@
 import '../scss/main.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('🧙‍♂️ Welcome to the Harry Potter API Site!');
+    console.log('🧙‍♂️ Welcome to the Harry Potter!');
+
+    const btn = document.querySelector('.hero__btn') as HTMLButtonElement;
+    const charactersSection = document.getElementById('characters') as HTMLElement;
+
+    btn.addEventListener('click', () => {
+        charactersSection.classList.remove('hidden');
+        btn.disabled = true;
+    });
     // Add your JavaScript code here
     // initHeroSection();
     // initCharacterCards();
