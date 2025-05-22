d12fab319c874a418f603bdc1f2e1c73bb960626
OlesiaKubska <kublesia0908@gmail.com>
2025-05-22 13:22:09 +0200

    Updates

--- harry-potter-api-site.b5b8a524.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{console.log("\uD83E\uDDD9‍♂️ Welcome to the Harry Potter!");let e=document.querySelector(".hero__btn"),t=document.getElementById("characters");e.addEventListener("click",()=>{t.classList.remove("hidden"),e.disabled=!0})});
-//# sourceMappingURL=harry-potter-api-site.b5b8a524.js.map
